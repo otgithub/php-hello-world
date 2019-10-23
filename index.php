352f55cf238b2903f7e4cde77407129ced9e4ef3
otgithub <oleg.torshin@ch.ibm.com>
2019-10-23 11:58:50 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Hello World for Red Hat OpenShift Session, Cloud University 2019</title>
+ </head>
+ <body>
+ <?php echo '<p>PHP Hello World for Red Hat OpenShift Session, Cloud University 2019</p>'; ?> 
+ </body>
+</html>
